530d35f46a5ea2a1d38c1a9a7d0dd07ec102d4b3
Einar <einargudnig@gmail.com>
2024-12-26 10:47:48 +0000

    improving typing practice

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -1,6 +1,7 @@
-import { motion } from "motion/react";
+import { delay, motion } from "motion/react";
 import { faker } from "@faker-js/faker";
 import React from "react";
+import { formatPercentage } from "../utils/helper";
 
 export function Practice() {
   console.log("Practice route");
@@ -13,6 +14,8 @@ export function Practice() {
 
       <Timer timeLeft={time} />
       <div className="practice">{words}</div>
+      <Restart onRestart={() => null} />
+      <Results errors={1} accuracyPercentage={99} total={8} />
     </div>
   );
 }
@@ -24,8 +27,60 @@ function Timer({ timeLeft }) {
       {timeLeft}
     </div>
   );
+}
 
+function Restart({ onRestart: handleRestart }: { onRestart: () => void }) {
+  const buttonRef = React.useRef<HTMLButtonElement>(null);
 
-function Restart() {
+  const handleClick = () => {
+    buttonRef.current?.blur();
+    console.log("Restart!");
+    handleRestart();
+  };
 
-  }
+  return (
+    <button ref={buttonRef} onClick={handleClick}>
+      Restart
+    </button>
+  );
+}
+
+function Results({
+  errors,
+  accuracyPercentage,
+  total,
+}: {
+  errors: number;
+  accuracyPercentage: number;
+  total: number;
+}) {
+  const initial = { opacity: 0 };
+  const animate = { opacity: 1 };
+  const duration = { duration: 0.3 };
+
+  return (
+    <motion.div className="results">
+      <motion.p
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 0.5 }}
+      >
+        Errors: {errors}
+      </motion.p>
+      <motion.p
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1 }}
+      >
+        Accuracy: {formatPercentage(accuracyPercentage)}%
+      </motion.p>
+      <motion.p
+        initial={initial}
+        animate={animate}
+        transition={{ ...duration, delay: 1.4 }}
+      >
+        Total: {total}
+      </motion.p>
+    </motion.div>
+  );
+}
